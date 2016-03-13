29b3505b8f286263548746e67dcfcde854d83a88
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-03-13 14:56:50 -0400

    Created srcset attributes to provide different images for different pixel sizes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,35 @@
 var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
+var rename = require('gulp-rename')
 
 gulp.task('default', function() {
+  var width = 540;
+  var height = 360;
   gulp.src('img-src/*.jpg')
   .pipe(imageResize({
-    width: 360,
-    height: 240
+    width: width,
+    height: height
+  }))
+  .pipe(rename({
+    suffix: '_1x'
+  }))
+  .pipe(gulp.dest('images'));
+  gulp.src('img-src/*.jpg')
+  .pipe(imageResize({
+    width: width * 2,
+    height: height * 2
+  }))
+  .pipe(rename({
+    suffix: '_2x'
+  }))
+  .pipe(gulp.dest('images'));
+  gulp.src('img-src/*.jpg')
+  .pipe(imageResize({
+    width: width * 3,
+    height: height * 3
+  }))
+  .pipe(rename({
+    suffix: '_3x'
   }))
   .pipe(gulp.dest('images'));
 });
